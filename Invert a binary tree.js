3b4d9927aaf3385cbcce660f47a3bfdaca1f72b3
Go7hic <gtfx0209@qq.com>
2015-10-17 23:46:47 +0800

    update

--- Invert a binary tree.js
+++ Invert a binary tree.js
@@ -25,10 +25,10 @@ var invertTree = function(root) {
 
     function swap(node){
         if(node !== null){
-            var tmp = node.left;
-            node.left = node.right;
-            node.right = tmp;
-            swap(node.left);
+            var tmp = node.left;//把左边的值先存起来
+            node.left = node.right; // 然后把左边的值设为右边的值
+            node.right = tmp; // 再把右边的值设置左边一开始的值
+            swap(node.left);// 在下一棵树循环这个操作
             swap(node.right);
         }
     }
